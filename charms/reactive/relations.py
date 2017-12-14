c2f4a68497ad7ddaf11fc18f66ff3f70499376d3
Liam Young <liam.young@canonical.com>
2017-12-14 14:42:00 +0000

    wheelhouse testing

--- charms/reactive/relations.py
+++ charms/reactive/relations.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with charm-helpers.  If not, see <http://www.gnu.org/licenses/>.
 
+# COOL NEW FEATURE
+
 import os
 import sys
 import importlib
